054dc86f8741842ff8c4314c408458bfbf74b833
zzmaxus <64233266+zzmaxus@users.noreply.github.com>
2020-10-26 11:37:45 -0500

    Addforce + MaxSpeed

---
+++ Assets/Scripts/RigidBodyCharacterController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidBodyCharacterController : MonoBehaviour
+{
+    [SerializeField]
+    private float accelerationforce = 10;
+
+    [SerializeField]
+    private float MaxSpeed = 5;
+
+    private new Rigidbody rigidbody;
+    private Vector2 input;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        var InputDirection = new Vector3(input.x, 0, input.y);
+
+        if(rigidbody.velocity.magnitude < MaxSpeed)
+        {
+            rigidbody.AddForce(InputDirection * accelerationforce);
+
+        }
+
+    }
+
+    private void Update()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+
+    }
+
+}
