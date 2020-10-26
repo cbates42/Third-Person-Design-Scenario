912adee5009f4568d416076c1a7b8b80c21813c5
zzmaxus <64233266+zzmaxus@users.noreply.github.com>
2020-10-26 14:21:16 -0500

    Physics Materials

--- Assets/Scripts/RigidBodyCharacterController.cs
+++ Assets/Scripts/RigidBodyCharacterController.cs
@@ -10,12 +10,17 @@ public class RigidBodyCharacterController : MonoBehaviour
     [SerializeField]
     private float MaxSpeed = 5;
 
+    [SerializeField]
+    private PhysicMaterial StoppingPhysicsMaterial, MovingPhysicsMaterial;
+
     private new Rigidbody rigidbody;
     private Vector2 input;
+    private new Collider collider;
 
     private void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        collider = GetComponent<Collider>();
     }
 
     private void FixedUpdate()
@@ -28,6 +33,15 @@ public class RigidBodyCharacterController : MonoBehaviour
 
         }
 
+        if (InputDirection.magnitude > 0)
+        {
+            collider.material = MovingPhysicsMaterial;
+        }
+
+        else
+        {
+            collider.material = StoppingPhysicsMaterial;
+        }    
     }
 
     private void Update()
